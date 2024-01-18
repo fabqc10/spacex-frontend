7908d224feccf9e6b4a0f3b9a33aebcbc62e7ef2
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-18 10:14:10 +0100

    httpGetRockets in requests file

--- src/app/requests.ts
+++ src/app/requests.ts
@@ -9,3 +9,8 @@ export async function httpGetLaunches(page: number){
     const response = await fetch(`${API_URL}/launches?page=${page}&limit4`);
     return await response.json();
 }
+
+export async function httpGetRockets(){
+    const response = await fetch(`${API_URL}/rockets`);
+    return await response.json();
+}
