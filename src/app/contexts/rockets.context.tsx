db52a7c7eb7ca5818d34a2578d013e3acfd303c1
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-18 10:37:58 +0100

    rockets context done

--- src/app/contexts/rockets.context.tsx
+++ src/app/contexts/rockets.context.tsx
@@ -1,1 +1,42 @@
 "use client";
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { httpGetRockets } from "../requests";
+
+type RocketProviderProps = {
+  children: ReactNode;
+};
+
+type RocketsContextType = {
+  rockets: Rocket[];
+};
+
+export const RocketsContext = createContext<RocketsContextType>({
+  rockets: [],
+});
+
+export const RocketProvider = ({ children }: RocketProviderProps) => {
+  const [rockets, setRockets] = useState<Rocket[]>([]);
+
+  const getRockets = useCallback(async () => {
+    const fetchedRockets = await httpGetRockets();
+    setRockets(fetchedRockets);
+  }, []);
+
+  useEffect(() => {
+    getRockets();
+  }, [getRockets]);
+
+  const value: RocketsContextType = {
+    rockets,
+  };
+
+  return (
+    <RocketsContext.Provider value={value}>{children}</RocketsContext.Provider>
+  );
+};
