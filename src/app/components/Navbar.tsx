712254f0338b8add3ae40cfe2c9abfadfce5bedc
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-01-25 19:45:13 +0100

    small fix in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     setActive(!active);
   };
   return (
-    <nav className="flex items-center p-4 bg-black-800 space-x-10 text-white p-15 ml-10">
+    <nav className="flex items-center p-4 bg-black-800 space-x-10 text-white p-15 ml-10 absolute">
       <div className="w-40 h-auto">
         <Link href={"/"}>
           <Logo />
