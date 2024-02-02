f1be215fa15485fedbbe45ef99ae0f9a46df235c
Fabio Quintero <fabio.quintero.cardona@gmail.com>
2024-02-02 18:48:12 +0100

    changed api url

--- src/app/requests.ts
+++ src/app/requests.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080/api/v1';
+const API_URL = 'https://spacexapi-java.onrender.com/api/v1';
 
 export async function httpGetCrew(page: number) {
     const response = await fetch(`${API_URL}/crew?page=${page}&limit=9`);
